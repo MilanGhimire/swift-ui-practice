649dba2fa8d65fa963dbcb9c8037e7ac4fbc550d
Milan Ghimire <milan.ghimire@ensue.us>
2023-08-01 21:43:48 +0545

    Created project

---
+++ SwiftUIPractice/SwiftUIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPracticeApp.swift
+//  SwiftUIPractice
+//
+//  Created by Ensue on 01/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
